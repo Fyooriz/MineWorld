7ff5db5c2227865f547f79bff49fcadfb1828269
Kairiz <293693813+Fyooriz@users.noreply.github.com>
2026-08-31 15:28:35 +0700

    fix(player): provide default p0 craft action

--- src/MineWorld.Playable/PlayerActionLayer.cs
+++ src/MineWorld.Playable/PlayerActionLayer.cs
@@ -1,4 +1,5 @@
 using MineWorld.Core.Crafting;
+using MineWorld.Core.Inventory;
 using MineWorld.Core.Player;
 
 namespace MineWorld.Playable;
@@ -8,10 +9,13 @@ internal sealed class PlayerActionLayer
     private readonly CraftingService _crafting;
     private readonly RecipeDefinition _p0Recipe;
 
-    public PlayerActionLayer(CraftingService crafting, RecipeDefinition p0Recipe)
+    public PlayerActionLayer(CraftingService? crafting = null, RecipeDefinition? p0Recipe = null)
     {
-        _crafting = crafting ?? throw new ArgumentNullException(nameof(crafting));
-        _p0Recipe = p0Recipe ?? throw new ArgumentNullException(nameof(p0Recipe));
+        _crafting = crafting ?? new CraftingService();
+        _p0Recipe = p0Recipe ?? new RecipeDefinition(
+            "p0:dirt-from-grass",
+            [new ItemStack("core:grass", 1)],
+            new ItemStack("core:dirt", 1));
     }
 
     public bool TryCraft(PlayerState player)
