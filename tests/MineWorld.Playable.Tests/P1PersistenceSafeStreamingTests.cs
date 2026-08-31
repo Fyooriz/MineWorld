82ed92ccb6c076c1e0093973a898277895cf05ad
Kairiz <293693813+Fyooriz@users.noreply.github.com>
2026-09-01 06:09:10 +0700

    test(p1): harden persistence streaming mutation coverage

--- tests/MineWorld.Playable.Tests/P1PersistenceSafeStreamingTests.cs
+++ tests/MineWorld.Playable.Tests/P1PersistenceSafeStreamingTests.cs
@@ -8,16 +8,19 @@ public sealed class P1PersistenceSafeStreamingTests
         var world = new VoxelWorld(seed: 12345, renderDistance: 1);
         var coordinates = new[]
         {
-            (X: -17, Y: 20, Z: -17),
-            (X: -16, Y: 21, Z: -16),
-            (X: -1, Y: 22, Z: 0),
-            (X: 0, Y: 23, Z: 0),
-            (X: 15, Y: 24, Z: 15),
-            (X: 16, Y: 25, Z: 16)
+            (X: -17, Z: -17),
+            (X: -16, Z: -16),
+            (X: -1, Z: 0),
+            (X: 0, Z: 0),
+            (X: 15, Z: 15),
+            (X: 16, Z: 16)
         };
 
         foreach (var coordinate in coordinates)
-            world.SetBlock(coordinate.X, coordinate.Y, coordinate.Z, VoxelWorld.Air);
+        {
+            var y = world.GetSurfaceHeight(coordinate.X, coordinate.Z);
+            world.SetBlock(coordinate.X, y, coordinate.Z, VoxelWorld.Air);
+        }
 
         Assert.Equal(coordinates.Length, world.BlockOverrides.Count);
 
@@ -25,22 +28,28 @@ public sealed class P1PersistenceSafeStreamingTests
         world.StreamAround(-0.5f, -0.5f);
 
         foreach (var coordinate in coordinates)
-            Assert.Equal(VoxelWorld.Air, world.GetBlock(coordinate.X, coordinate.Y, coordinate.Z));
+        {
+            var y = world.GetSurfaceHeight(coordinate.X, coordinate.Z);
+            Assert.Equal(VoxelWorld.Air, world.GetBlock(coordinate.X, y, coordinate.Z));
+        }
     }
 
     [Fact]
     public void PersistedOverridesSurviveFullEvictionAndDiskReload()
     {
         var world = new VoxelWorld(seed: 2468, renderDistance: 1);
-        var changes = new[]
+        var coordinates = new[]
         {
-            (X: -33, Y: 18, Z: 7, Block: VoxelWorld.Air),
-            (X: 31, Y: 19, Z: -32, Block: VoxelWorld.Dirt),
-            (X: 16, Y: 20, Z: 16, Block: VoxelWorld.Stone)
+            (X: -33, Z: 7),
+            (X: 31, Z: -32),
+            (X: 16, Z: 16)
         };
 
-        foreach (var change in changes)
-            world.SetBlock(change.X, change.Y, change.Z, change.Block);
+        foreach (var coordinate in coordinates)
+        {
+            var y = world.GetSurfaceHeight(coordinate.X, coordinate.Z);
+            world.SetBlock(coordinate.X, y, coordinate.Z, VoxelWorld.Air);
+        }
 
         world.StreamAround(2048.5f, 2048.5f);
 
@@ -50,8 +59,11 @@ public sealed class P1PersistenceSafeStreamingTests
             WorldPersistence.Save(world, path);
             var loaded = WorldPersistence.Load(path, renderDistance: 1);
 
-            foreach (var change in changes)
-                Assert.Equal(change.Block, loaded.GetBlock(change.X, change.Y, change.Z));
+            foreach (var coordinate in coordinates)
+            {
+                var y = world.GetSurfaceHeight(coordinate.X, coordinate.Z);
+                Assert.Equal(VoxelWorld.Air, loaded.GetBlock(coordinate.X, y, coordinate.Z));
+            }
         }
         finally
         {
